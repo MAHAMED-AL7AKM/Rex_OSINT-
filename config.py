6a25580b7fd37cffeb720a22553219bd61000368
mohamed El7akm <mhmdalhakmlfkalarqam@gmail.com>
2026-09-11 02:19:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Rex OSINT - Configuration"""
+
+VERSION = "1.0.0"
+AUTHOR = "Your Name"
+GITHUB = "https://github.com/MAHAMED-AL7AKM/Rex_OSINT"
+
+# Colors
+from colorama import Fore, Style
+RED     = Fore.RED + Style.BRIGHT
+GREEN   = Fore.GREEN + Style.BRIGHT
+YELLOW  = Fore.YELLOW + Style.BRIGHT
+BLUE    = Fore.CYAN + Style.BRIGHT
+MAGENTA = Fore.MAGENTA + Style.BRIGHT
+WHITE   = Fore.WHITE + Style.BRIGHT
+RESET   = Style.RESET_ALL
+
+# Paths
+RESULTS_DIR = "results"
+TIMEOUT = 10
